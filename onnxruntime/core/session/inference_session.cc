228fef38552734e24ef09a45626682d66b2895be
vamshikrishna <vamshikrishna@in.ibm.com>
2023-04-10 03:19:08 -0500

    merge issue.

--- onnxruntime/core/session/inference_session.cc
+++ onnxruntime/core/session/inference_session.cc
@@ -1036,13 +1036,8 @@ Status InferenceSession::LoadOrtModel(const void* model_data, int model_data_len
   });
 }
 
-<<<<<<< HEAD
-Status InferenceSession::LoadOrtModelWithLoader(std::function<Status()> load_ort_format_model_bytes) {
-  static_assert(FLATBUFFERS_LITTLEENDIAN, "ORT format only supports little-endian machines");
-=======
 Status InferenceSession::LoadOrtModel(std::function<Status()> load_ort_format_model_bytes) {
 //  static_assert(FLATBUFFERS_LITTLEENDIAN, "ORT format only supports little-endian machines");
->>>>>>> origin/tvkaibranch3
 
   std::lock_guard<onnxruntime::OrtMutex> l(session_mutex_);
 
